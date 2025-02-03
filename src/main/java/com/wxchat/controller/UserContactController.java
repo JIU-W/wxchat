ce4aa22a5ae98b7ff4473ef1a4d13a86ac5d847b
JIU-W <2661727280@qq.com>
2025-02-03 23:01:15 +0800

    搜索联系人信息

--- src/main/java/com/wxchat/controller/UserContactController.java
+++ src/main/java/com/wxchat/controller/UserContactController.java
@@ -58,6 +58,7 @@ public class UserContactController extends ABaseController {
     public ResponseVO search(HttpServletRequest request, @NotEmpty String contactId) {
         TokenUserInfoDto tokenUserInfoDto = getTokenUserInfo(request);
         //精确搜索用户的联系人信息(用户的好友或者用户加入的群组)
+        //这里搜索出来的联系人不一定就是好友，也有可能是非好友，也有可能被好友删除等等，要根据status去具体判断。
         UserContactSearchResultDto resultDto =
                 userContactService.searchContact(tokenUserInfoDto.getUserId(), contactId);
         return getSuccessResponseVO(resultDto);
