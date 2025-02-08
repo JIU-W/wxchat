752a1984c557c3895cd49a41e2fbc2dab32ed7aa
JIU-W <2661727280@qq.com>
2025-02-08 14:01:37 +0800

    获取用户信息

---
+++ src/main/java/com/wxchat/controller/UserInfoController.java
@@ -0,0 +1,88 @@
+package com.wxchat.controller;
+
+import com.wxchat.annotation.GlobalInterceptor;
+import com.wxchat.entity.constants.Constants;
+import com.wxchat.entity.dto.TokenUserInfoDto;
+import com.wxchat.entity.po.UserInfo;
+import com.wxchat.entity.vo.ResponseVO;
+import com.wxchat.entity.vo.UserInfoVO;
+import com.wxchat.service.UserInfoService;
+import com.wxchat.utils.CopyTools;
+import com.wxchat.utils.StringTools;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import java.io.IOException;
+
+/**
+ * 账号信息 Controller
+ */
+@RestController("userInfoController")
+@RequestMapping("/userInfo")
+public class UserInfoController extends ABaseController {
+
+    @Resource
+    private UserInfoService userInfoService;
+
+    //@Resource
+    //private ChannelContextUtils channelContextUtils;
+
+    /**
+     * 获取用户信息
+     * @param request
+     * @return
+     */
+    @RequestMapping("/getUserInfo")
+    @GlobalInterceptor
+    public ResponseVO getUserInfo(HttpServletRequest request) {
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfo(request);
+        UserInfo userInfo = userInfoService.getUserInfoByUserId(tokenUserInfoDto.getUserId());
+        UserInfoVO userInfoVO = CopyTools.copy(userInfo, UserInfoVO.class);
+        userInfoVO.setAdmin(tokenUserInfoDto.getAdmin());
+        return getSuccessResponseVO(userInfoVO);
+    }
+
+/*
+    @RequestMapping("/saveUserInfo")
+    @GlobalInterceptor
+    public ResponseVO saveUserInfo(HttpServletRequest request, UserInfo userInfo, MultipartFile avatarFile, MultipartFile avatarCover) throws IOException {
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfo(request);
+        userInfo.setUserId(tokenUserInfoDto.getUserId());
+        userInfo.setPassword(null);
+        userInfo.setStatus(null);
+        userInfo.setCreateTime(null);
+        userInfo.setLastLoginTime(null);
+        this.userInfoService.updateUserInfo(userInfo, avatarFile, avatarCover);
+        if (!tokenUserInfoDto.getNickName().equals(userInfo.getNickName())) {
+            tokenUserInfoDto.setNickName(userInfo.getNickName());
+            resetTokenUserInfo(request, tokenUserInfoDto);
+        }
+        return getUserInfo(request);
+    }
+
+    @RequestMapping("/updatePassword")
+    @GlobalInterceptor
+    public ResponseVO updatePassword(HttpServletRequest request, @NotEmpty @Pattern(regexp = Constants.REGEX_PASSWORD) String password) {
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfo(request);
+        UserInfo userInfo = new UserInfo();
+        userInfo.setPassword(StringTools.encodeByMD5(password));
+        this.userInfoService.updateUserInfoByUserId(userInfo, tokenUserInfoDto.getUserId());
+        //channelContextUtils.closeContext(tokenUserInfoDto.getUserId());
+        return getSuccessResponseVO(null);
+    }
+
+    @RequestMapping("/logout")
+    @GlobalInterceptor
+    public ResponseVO logout(HttpServletRequest request) {
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfo(request);
+        //关闭ws
+        //channelContextUtils.closeContext(tokenUserInfoDto.getUserId());
+        return getSuccessResponseVO(null);
+    }*/
+
+}
