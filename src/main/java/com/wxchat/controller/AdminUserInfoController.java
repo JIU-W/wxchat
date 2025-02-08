faa1869769dc7ff41644aa875ab83528c15aab1e
JIU-W <2661727280@qq.com>
2025-02-08 17:09:16 +0800

    管理后台

---
+++ src/main/java/com/wxchat/controller/AdminUserInfoController.java
@@ -0,0 +1,46 @@
+package com.wxchat.controller;
+
+import com.wxchat.annotation.GlobalInterceptor;
+import com.wxchat.entity.query.UserInfoQuery;
+import com.wxchat.entity.vo.PaginationResultVO;
+import com.wxchat.entity.vo.ResponseVO;
+import com.wxchat.service.UserInfoService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@RestController("adminUserInfoController")
+@RequestMapping("/admin")
+public class AdminUserInfoController extends ABaseController {
+
+    @Resource
+    private UserInfoService userInfoService;
+
+    @RequestMapping("/loadUser")
+    @GlobalInterceptor(checkAdmin = true)
+    public ResponseVO loadUser(UserInfoQuery userInfoQuery) {
+        userInfoQuery.setOrderBy("create_time desc");
+        PaginationResultVO resultVO = userInfoService.findListByPage(userInfoQuery);
+        return getSuccessResponseVO(resultVO);
+    }
+
+
+    @RequestMapping("/updateUserStatus")
+    @GlobalInterceptor(checkAdmin = true)
+    public ResponseVO updateUserStatus(@NotNull Integer status,
+                                       @NotEmpty String userId) {
+        userInfoService.updateUserStatus(status, userId);
+        return getSuccessResponseVO(null);
+    }
+
+    @RequestMapping("/forceOffLine")
+    @GlobalInterceptor(checkAdmin = true)
+    public ResponseVO forceOffLine(@NotEmpty String userId) {
+        userInfoService.forceOffLine(userId);
+        return getSuccessResponseVO(null);
+    }
+
+}
