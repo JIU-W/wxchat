1c8279ee4121f01d8ccdf49da722292ebe9e1a2c
JIU-W <2661727280@qq.com>
2025-02-24 17:28:08 +0800

    代码补充

--- src/main/java/com/wxchat/WxChatApplication.java
+++ src/main/java/com/wxchat/WxChatApplication.java
@@ -1,5 +1,8 @@
 package com.wxchat;
 
+import com.wxchat.entity.config.AppConfig;
+import com.wxchat.entity.constants.Constants;
+import com.wxchat.spring.ApplicationContextProvider;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,13 +25,22 @@ public class WxChatApplication {
         SpringApplication.run(WxChatApplication.class, args);
     }
 
-    /*@Bean
+    /**
+     * 文件上传临时路径
+     * @return
+     */
+    @Bean
     @DependsOn({"applicationContextProvider"})
     MultipartConfigElement multipartConfigElement() {
+        //获取名为appConfig的 Bean
         AppConfig appConfig = (AppConfig) ApplicationContextProvider.getBean("appConfig");
+        //MultipartConfigFactory 是Spring用于配置 multipart 文件上传的工厂类
         MultipartConfigFactory factory = new MultipartConfigFactory();
+        //设置临时文件存储路径
         factory.setLocation(appConfig.getProjectFolder() + Constants.FILE_FOLDER_TEMP);
+        //创建并返回 MultipartConfigElement 对象
+        //用于支持文件上传功能（例如设置最大文件大小、临时存储路径等）
         return factory.createMultipartConfig();
-    }*/
+    }
 
 }
